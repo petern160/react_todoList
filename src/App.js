013c8dff9d28cd1a5a4fc78bf66e3bbb5182e320
petern160 <peternguyen160@gmail.com>
2019-04-21 16:14:58 -0700

    added uuid delete and submit functionalities are working

--- src/App.js
+++ src/App.js
@@ -3,22 +3,23 @@ import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
 import './App.css';
+import uuid from 'uuid';
 
 class App extends Component {
   state = {
     todos : [
       {
-        id: 1,
+        id: uuid.v4(),
         title: 'Take out the trash',
         completed: false
       },
       {
-        id: 2,
+        id: uuid.v4(),
         title: 'Dinner with wife',
         completed: true
       },
       {
-        id: 3,
+        id: uuid.v4(),
         title: 'Meeting with boss',
         completed: false
       }
@@ -40,12 +41,22 @@ delTodo = (id) => {
   this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] })
 }
 
+//Add Todo
+addTodo = (title) => {
+  const newTodo = {
+    id: uuid.v4(),
+    title: title,
+    completed: false
+  }
+  this.setState({ todos: [...this.state.todos, newTodo] })
+}
+
   render() {
     return (
       <div className="App">
       <div className="container">
       <Header />
-      <AddTodo />
+      <AddTodo addTodo={this.addTodo}/>
         <Todos todos={this.state.todos} markComplete={this.markComplete}
         delTodo={this.delTodo} />
         </div>
