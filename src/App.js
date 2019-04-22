032ba3b9e996b69a9d03ff720e395e7ed7d873fb
petern160 <peternguyen160@gmail.com>
2019-04-21 17:13:13 -0700

    about to import new data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
+import About from './components/pages/About';
 import './App.css';
 import uuid from 'uuid';
 
@@ -53,14 +55,21 @@ addTodo = (title) => {
 
   render() {
     return (
+      <Router>
       <div className="App">
       <div className="container">
       <Header />
-      <AddTodo addTodo={this.addTodo}/>
+      <Route exact path="/" render={props => (
+        <React.Fragment>
+        <AddTodo addTodo={this.addTodo}/>
         <Todos todos={this.state.todos} markComplete={this.markComplete}
         delTodo={this.delTodo} />
+        </React.Fragment>
+      )} />
+      <Route path="/about" component={About} />
         </div>
       </div>
+      </Router>
     );
   }
 }
